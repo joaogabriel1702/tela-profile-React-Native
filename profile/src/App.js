4767a09a41f2244af6b377ea871595cf1ba95d73
João Gabriel <joaogabrielcsoares@hotmail.com>
2022-06-16 22:27:39 -0300

    criação do button para ir para o meu github

--- profile/src/App.js
+++ profile/src/App.js
@@ -6,21 +6,51 @@ import {
   SafeAreaView,
   StatusBar,
   Image,
+  Pressable,
+  Linking,
 } from 'react-native';
 
 const colorGithub = '#010409';
-const imageProfileGithub = 'https://avatars.githubusercontent.com/u/38577266?v=4';
+const colorFontGithub = '#C9D1D9';
+const colorDarkFontGithub = '#4F565E';
+const imageProfileGithub =
+  'https://avatars.githubusercontent.com/u/38577266?v=4';
+const urlToMyGithub = 'https://github.com/joaogabriel1702';
 
 const App = () => {
+  const handlePressGoToGithub = async () => {
+    const res = await Linking.canOpenURL(urlToMyGithub);
+    if (res) {
+      await Linking.openURL(urlToMyGithub);
+    }
+  };
+
   return (
     <SafeAreaView style={style.container}>
       <StatusBar backgroundColor={colorGithub} barStyle="light-content" />
       <View style={style.content}>
         <Image
-          accessibilityLabel='João Pirata'
+          accessibilityLabel="João Pirata"
           style={style.avatar}
           source={{uri: imageProfileGithub}}
         />
+        <Text
+          accessibilityLabel="Nome: João Gabriel"
+          style={[style.defaultText, style.name]}>
+          João Gabriel
+        </Text>
+        <Text
+          accessibilityLabel="Nickname: joaogabriel1702"
+          style={[style.defaultText, style.nickname]}>
+          joaogabriel1702
+        </Text>
+        <Pressable onPress={handlePressGoToGithub}>
+          <View style={style.button}>
+            <Text style={[style.defaultText, style.textButton]}>
+              Open in Github
+            </Text>
+          </View>
+        </Pressable>
       </View>
     </SafeAreaView>
   );
@@ -38,6 +68,7 @@ const style = StyleSheet.create({
 
   content: {
     alignItems: 'center',
+    padding: 20,
   },
 
   avatar: {
@@ -46,5 +77,32 @@ const style = StyleSheet.create({
     borderRadius: 100,
     borderColor: 'white',
     borderWidth: 2,
+  },
+
+  defaultText: {
+    color: colorFontGithub,
+  },
+
+  name: {
+    marginTop: 20,
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+
+  nickname: {
+    color: colorDarkFontGithub,
+    fontSize: 18,
+  },
+
+  button: {
+    marginTop: 20,
+    backgroundColor: colorDarkFontGithub,
+    borderRadius: 10,
+    padding: 20,
+  },
+
+  textButton: {
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
